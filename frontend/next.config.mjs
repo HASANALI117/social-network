42112d8512f626fb107f77e701ffb20e4bc75145
HASANALI117 <hassanali61911@gmail.com>
2025-05-25 23:47:07 +0300

    Update next.config.mjs to change image hostname and API destination

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,22 +3,22 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '9000', // As per the error message
-        pathname: '/images/uploads/**',
+        protocol: "http",
+        hostname: "minio_local_storage",
+        port: "9000", // As per the error message
+        pathname: "/images/uploads/**",
       },
       {
-        protocol: 'http',
-        hostname: 'example.com',
+        protocol: "http",
+        hostname: "example.com",
       },
     ],
   },
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'http://localhost:8080/api/:path*',
+        source: "/api/:path*",
+        destination: "http://backend_app:8080/api/:path*",
       },
     ];
   },
